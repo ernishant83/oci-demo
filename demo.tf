772686c54dceda7c86ac9b78ef1c25a74b781eb2
ernishant83 <ernishant83@gmail.com>
2023-07-07 11:09:10 -0400

    Fixed Typo

--- demo.tf
+++ demo.tf
@@ -4,6 +4,9 @@ provider "oci" {
 variable "bucket_namespace" {
     default = "idamb95plss4"
 }
+variable "compartment_ocid" {
+    default = "ocid1.compartment.oc1..aaaaaaaaelfomo57ez3eikd6vjcsardqpk7egfgfv4skcix4fb76qbwywj4a"
+}
 resource "oci_objectstorage_bucket" "create_bucket" {
     # required
     compartment_id = var.compartment_ocid
